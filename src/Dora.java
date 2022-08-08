2dec3adfb8ea090528d83c4cd64a8a1db81301fe
NeerajaSiripalli <neeraja.siripalli@ivycomptech.com>
2022-08-08 21:35:19 +0530

    another commit

--- src/Dora.java
+++ src/Dora.java
@@ -1,2 +1,21 @@
+import java.util.*;
 public class Dora {
-}
+    public static void main(String args[]){
+        Scanner in =new Scanner(System.in);
+        int n =in.nextInt();
+        int m =in.nextInt();
+        int k =in.nextInt();
+
+        if(k<=m) {
+             System.out.println("Yes");
+        }
+            else if((k%m)==1 || (k%m)==0) {
+            System.out.println("Yes");
+        }
+            else {
+            System.out.println("No");
+        }
+
+
+                }
+            }
