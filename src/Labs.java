a2f0c740a7cca96092218723656e2a5ad3381b35
NeerajaSiripalli <neeraja.siripalli@ivycomptech.com>
2022-08-08 15:41:56 +0530

    second commit

--- src/Labs.java
+++ src/Labs.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Labs {
+public class Labs {
+    public static void main(String args[]){
+        int x,y,z;
+        int n;
+     if(int n<=int x&&int n<=int y&&int n<=int z){
+         if(int(x-n)<int(y-n)&&int(y-n)<int(z-n){
+         System.out.println("L1");
+         }
+         else if(int(x-n)>int(y-n)&&int(y-n)>int(z-n){
+             System.out.println("L3");
+             }
+         else {
+             System.out.println("L2");
+         }
+         }
+     else if(int n>int x&&int n>int y&&int n>int z){
+         System.out.println("no lab is suitable");
+     }
+     else {
+         if(int n< int x){
+             if(int(y-n)<int (z-n))
+                 System.out.println("L2");
+             else
+                 System.out.println("L3");
+            }
+
+     }
+     }
+
 }
