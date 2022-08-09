0921e79bc641f9674cd3b3758c77f6620ac95b15
NeerajaSiripalli <neeraja.siripalli@ivycomptech.com>
2022-08-09 12:33:37 +0530

    fifth commit

---
+++ src/StringOperations.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class StringOperations {
+    public static void main(){
+        String s1 = "Neeraja";
+        String s2 = "Siripalli";
+        char c;
+       String  k =" ";
+        System.out.println(s1.length());
+        System.out.println(s2.length());
+        for(int i=0;i<s1.length();i++){
+            c = s1.charAt(i);
+            k = c+k;
+        }
+        System.out.println(k);
+        System.out.println(s1.concat(s2));
+        System.out.println(s1.equals(s2));
+
+    }
+}
